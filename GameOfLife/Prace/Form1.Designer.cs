d450b9b740b6017e91943dee7a36bcd617213bd6
MrVendys <vasik.ptak@gmail.com>
2024-07-17 18:14:27 +0200

    Update GameOfLife
    Add comments, change buttons names

--- GameOfLife/Prace/Form1.Designer.cs
+++ GameOfLife/Prace/Form1.Designer.cs
@@ -30,12 +30,12 @@
         {
             this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.vygenerujBtn = new System.Windows.Forms.Button();
             this.Cislo = new System.Windows.Forms.NumericUpDown();
-            this.button2 = new System.Windows.Forms.Button();
+            this.startBtn = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.pauseBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.Cislo)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,16 +49,16 @@
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
             // 
-            // button1
+            // vygenerujBtn
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(488, 51);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(115, 42);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Vygeneruj";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.vygenerujBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.vygenerujBtn.Location = new System.Drawing.Point(488, 51);
+            this.vygenerujBtn.Name = "vygenerujBtn";
+            this.vygenerujBtn.Size = new System.Drawing.Size(115, 42);
+            this.vygenerujBtn.TabIndex = 2;
+            this.vygenerujBtn.Text = "Vygeneruj";
+            this.vygenerujBtn.UseVisualStyleBackColor = true;
+            this.vygenerujBtn.Click += new System.EventHandler(this.vygenerujBtn_Click);
             // 
             // Cislo
             // 
@@ -77,54 +77,54 @@
             0,
             0});
             // 
-            // button2
+            // startBtn
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button2.Location = new System.Drawing.Point(488, 115);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(115, 43);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Start";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.startBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.startBtn.Location = new System.Drawing.Point(488, 115);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(115, 43);
+            this.startBtn.TabIndex = 4;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
             // 
             // timer1
             // 
-            this.timer1.Interval = 500;
+            this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // button3
+            // stopBtn
             // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button3.Location = new System.Drawing.Point(488, 233);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(115, 41);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Stop";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.stopBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.stopBtn.Location = new System.Drawing.Point(488, 233);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(115, 41);
+            this.stopBtn.TabIndex = 5;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
             // 
-            // button4
+            // pauseBtn
             // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button4.Location = new System.Drawing.Point(488, 175);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(115, 40);
-            this.button4.TabIndex = 6;
-            this.button4.Text = "Pause";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.pauseBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.pauseBtn.Location = new System.Drawing.Point(488, 175);
+            this.pauseBtn.Name = "pauseBtn";
+            this.pauseBtn.Size = new System.Drawing.Size(115, 40);
+            this.pauseBtn.TabIndex = 6;
+            this.pauseBtn.Text = "Pause";
+            this.pauseBtn.UseVisualStyleBackColor = true;
+            this.pauseBtn.Click += new System.EventHandler(this.pauseBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(615, 495);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pauseBtn);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
             this.Controls.Add(this.Cislo);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.vygenerujBtn);
             this.Controls.Add(this.panel1);
             this.DoubleBuffered = true;
             this.Name = "Form1";
@@ -137,12 +137,12 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button vygenerujBtn;
         private System.Windows.Forms.NumericUpDown Cislo;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button startBtn;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.Button pauseBtn;
     }
 }
 
--- GameOfLife/Prace/Form1.cs
+++ GameOfLife/Prace/Form1.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Prace
 {
     public partial class Form1 : Form
     {
+        //Promenne
+        //pole je to, ktere se vykresluje
+        //pole2 je sekundarni, ktere slouzi pri vypoctu sousedu
         Bunka[,] pole;
         Bunka[,] pole2;
         bool odpocet = false;
@@ -20,9 +16,9 @@ namespace Prace
             InitializeComponent();
             GenerujBunky();
         }
+        //Naplneni poli bunkami
         public void GenerujBunky()
         {
-            int pocet = 0;
             pole = new Bunka[47, 47];
             pole2 = new Bunka[47, 47];
             for (int i = 0; i < 47; i++)
@@ -31,10 +27,10 @@ namespace Prace
                 {
                     pole[i, j] = new Bunka(false, 10);
                     pole2[i, j] = new Bunka(false, 10);
-                    pocet++;
                 }
             }
         }
+        //Vykreslovani bunek na panel podle vlastni funkce ve tride "Bunka.cs"
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
             for (int i = 1; i < 47 - 1; i++)
@@ -45,6 +41,7 @@ namespace Prace
                 }
             }
         }
+        //Reseni konfigurace bunek uzivatelem
         private void panel1_MouseClick(object sender, MouseEventArgs e)
         {
             int x = e.X / 10;
@@ -52,8 +49,10 @@ namespace Prace
             pole[x, y].ziva = !pole[x, y].ziva;
             panel1.Refresh();
         }
-        private void button1_Click(object sender, EventArgs e)
+        //Generace bunek na panel
+        private void vygenerujBtn_Click(object sender, EventArgs e)
         {
+            //Resetovani vsech bunek (na mrtve -> bile)
             for (int i = 0; i < 47; i++)
             {
                 for (int j = 0; j < 47; j++)
@@ -61,13 +60,13 @@ namespace Prace
                     pole[i, j].ziva = false;
                 }
             }
+            //Podle inputu spočítání kolik bude zivych bunek
             Random r = new Random();
             double a = (double)Cislo.Value;
-            Console.WriteLine(a.ToString());
-            double b = (a / 100) * ((pole.GetLength(1)-1) * (pole.GetLength(0)-1));
-            Console.WriteLine(b.ToString());
+            double b = (a / 100) * ((pole.GetLength(1) - 1) * (pole.GetLength(0) - 1));
 
-          for (int i = 0; i < b; i++)
+            //Oziveni bunek, pokud se narazi jiz na zivou, zkusi znovu
+            for (int i = 0; i < b; i++)
             {
                 int r1 = r.Next(1, 47);
                 int r2 = r.Next(1, 47);
@@ -83,21 +82,15 @@ namespace Prace
             }
             panel1.Refresh();
         }
-        private void button2_Click(object sender, EventArgs e)
+        //Start hry, zapnuti timeru
+        private void startBtn_Click(object sender, EventArgs e)
         {
             timer1.Start();
             panel1.Refresh();
         }
+        //Kazdy tick timeru (1s) se prepocita, kolik ma bunka soousedu a podle pravidel bud zustane ziva, ozivi se, nebo umre a obnovi se panel
         private void timer1_Tick(object sender, EventArgs e)
         {
-            pole2 = new Bunka[47,47];
-            for (int i = 0; i < 47; i++)
-            {
-                for (int j = 0; j < 47; j++)
-                {
-                    pole2[i, j] = new Bunka(false, 10);
-                }
-            }
             for (int i = 1; i < 46; i++)
             {            
                 for (int j = 1; j < 46; j++)
@@ -131,9 +124,11 @@ namespace Prace
             pole = pole2;
             panel1.Refresh();
         }
+        //Funkce na vypocitani poctu sousedu
         public int GetPocetSousedu(int x, int y)
         {
             int pocet = 0;
+            //Ctverec kolem bunky
             for (int i = -1; i < 2; i++)
             {
                 for (int j = -1; j < 2; j++)
@@ -149,7 +144,23 @@ namespace Prace
             }
             return pocet;
         }
-        private void button3_Click(object sender, EventArgs e) 
+        //Pozastavi timer a bunky prestanou umirat, ozivovat se
+        private void pauseBtn_Click(object sender, EventArgs e)
+        {
+            
+            if (odpocet == false)
+            {
+                timer1.Enabled = false;
+            }
+            else
+            {
+                timer1.Enabled = true;
+            }
+            odpocet = !odpocet;
+        }
+
+        //Nastavi vsechny bunky na mrtve -> Reset hry
+        private void stopBtn_Click(object sender, EventArgs e)
         {
             timer1.Stop();
             for (int i = 1; i < 47 - 1; i++)
@@ -160,19 +171,6 @@ namespace Prace
                 }
             }
             panel1.Refresh();
-        }
-        private void button4_Click(object sender, EventArgs e)
-        {
-            if (odpocet == false)
-            {
-                timer1.Enabled = false;
-            }
-            else
-            {
-                timer1.Enabled = true;
-            }
-            odpocet = !odpocet;
-            
         }
     }
 }
